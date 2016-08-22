8b474efc2454c6353dde8ff3861dfb2b5864ce5b
David Nichols <davenich@gmail.com>
2016-08-22 15:25:32 -0400

    Housekeeping

--- davidneedsto/webtask.js
+++ davidneedsto/webtask.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * Represents an Event to be scheduled in Google Calendar
+ */
 function Event(matchedPhrase) {
   if (matchedPhrase) {
     this.who = matchedPhrase[1];
@@ -9,24 +12,33 @@ function Event(matchedPhrase) {
   }
 }
 
+/**
+ * Represents a "Chat" as designated in Gmail and a list of "Events" that are parsed from the message body
+ */
 function Chat(messageBody) {
   if (messageBody) {
     this.messageBody = messageBody;
-    this.events = [];
   } else {
     throw new Error('messageBody is required.');
   }
 }
 
-Chat.prototype.getMatches = function () {
+/**
+ * Get a list of Events from the message body
+ */
+Chat.prototype.getEvents = function () {
   // Matches (someone) needs to "(some action)" on "(some date)" at "(time)"
   var taskMatcher = /(\w+)\s\bneeds\b\s\bto\b\s"([\w\s]+)"\s\bon\b\s"([a-z\s0-9,]+)"\s\bat\b\s"([amp\s0-9:]+)"/;
   var matcher = new RegExp(taskMatcher, 'gi');
-  
-  this.messageBody.replace(matcher, function (match) {
-    this.events.push(new Event(matcher.exec(match)));
-    matcher.lastIndex = 0;
-  }.bind(this));
+  var events = [];
 
-  return this.events;
+  // Push an Event onto the event list and reset the lastIndex property
+  function addEvent(match) {
+    var event = new Event(matcher.exec(match));
+    if (event.valid) events.push(event);
+    matcher.lastIndex = 0;
+  }
+
+  this.messageBody.replace(matcher, addEvent.bind(this));
+  return events;
 };
